f2c430a5a23af22d58776346777d8da90f905057
Nick Armfield <bizzyn3@gmail.com>
2023-06-09 19:18:03 +0300

    feat: add app types

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,6 +2,8 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -10,7 +12,7 @@ class App {
     start() {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+            ?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
