91bbe9a4e0567c01d2e4955ff4c04631d21f8231
Nick Armfield <bizzyn3@gmail.com>
2023-06-09 16:50:15 +0300

    chore: add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7e02b7510dcb4499a8a10d6ac77f121e', // получите свой ключ https://newsapi.org/
         });
     }
 }
