33020ad1b0d7b97f31413bfff5fb8546e54a6ca4
Nick Armfield <bizzyn3@gmail.com>
2023-06-09 19:26:07 +0300

    feat: add enum

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,25 +1,31 @@
 import { SourceType } from '../../../types';
 import './sources.css';
 
+enum SourcesSelectors {
+    TEMPLATE = '#sourceItemTemp',
+    NAME = '.source__item-name',
+    ITEM = '.source__item',
+    CONTAINER = '.sources',
+}
 class Sources {
     draw(data: Array<SourceType>) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const sourceItemTemp = document.querySelector(SourcesSelectors.TEMPLATE) as HTMLTemplateElement;
 
         data.forEach((item) => {
             const sourceClone = sourceItemTemp?.content.cloneNode(true) as Element;
 
-            const name = sourceClone.querySelector('.source__item-name');
+            const name = sourceClone.querySelector(SourcesSelectors.NAME);
             if (name) {
                 name.textContent = item.name;
             }
 
-            sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+            sourceClone.querySelector(SourcesSelectors.ITEM)?.setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources')?.append(fragment);
+        document.querySelector(SourcesSelectors.CONTAINER)?.append(fragment);
     }
 }
 
